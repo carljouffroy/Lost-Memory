6fe2f0bd4c66e33533eeaed91577f014c3cfbe35
carljouffroy <carljouffroy@gmail.com>
2014-05-11 14:30:23 +0100

    Second commit
    clean up commit

---
+++ Assets/DoorOpen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	// Update is called once per frame
+	void Onmousedown()
+	{
+		rigidbody.AddForce (-transform.forward * 500);
+		rigidbody.useGravity = true;
+	}
+}
